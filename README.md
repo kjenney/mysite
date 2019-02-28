fc2e771459c3cb81795bfdcce369bc03daf1c3fd
Ken Jenney <kjenney@thrivehive.com>
2019-02-27 21:10:46 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,17 @@
+# mysite
+
+My site built on React:
+
+http://astronautweb.co/react-static-sites/
+
+## Build and Deploy
+
+```
+yarn install
+yarn build
+```
+
+# Deploy
+
+Copy the build folder to my S3 Bucket...
+                ...these aren't the Droids you're looking for
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Post from './Post';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">
+              <img src={logo} className="App-logo" alt="logo" />
+            </Link>
+            <Link to="/">Home</Link> <Link to="/post/">Post</Link>
+          </header>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/post/" component={Post} />
+          </Switch>
+        </div>
+    </BrowserRouter>
+    );
+  }
+}
+
+export default App;
